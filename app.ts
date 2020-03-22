1c8f05f010d063bfa385b98c901eb3bc525760c9
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-03-22 16:32:45 +0200

    add function

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('Your code goes here');
+// without types the func concats the numbers, resulting to wrong result
+// with types the compiler returns error
+
+function add(n1: number, n2:number) {
+  return n1 + n2;
+}
+
+const number1 = '5';
+const number2 = 2.5;
+
+const res = add(number1, number2);
+
+console.log(res)
