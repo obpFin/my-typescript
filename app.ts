08773d91c3381c5deb934ed44fc4268af509a81b
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-03-23 17:43:10 +0200

    type inference

--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
-// without types the func concats the numbers, resulting to wrong result
-// with types the compiler returns error
+// without types the funcion concats the numbers, resulting to wrong result
+// with types the compiler returns error if parameter is wrong type
 
-function add(n1: number, n2:number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
-const number1 = '5';
+let number1: number;  // type should be only declared when the var is not instanciated
+number1 = 5;
+
+// TypeScript guesses the types with type inference when the variables are declared with a value
 const number2 = 2.5;
+const printResult = true;
+const resultPhrase = 'Result is ';
 
-const res = add(number1, number2);
-
-console.log(res)
+add(number1, number2, printResult, resultPhrase);
