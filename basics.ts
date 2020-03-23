6597d9e27ce3df24d3593c1ea0c346e5b8660c4d
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-03-23 18:51:45 +0200

    more basic types

---
+++ basics.ts
@@ -0,0 +1,21 @@
+// without types the funcion concats the numbers, resulting to wrong result
+// with types the compiler returns error if parameter is wrong type
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+let number1: number;  // type should be only declared when the var is not instanciated
+number1 = 5;
+
+// TypeScript guesses the types with type inference when the variables are declared with a value
+const number2 = 2.5;
+const printResult = true;
+const resultPhrase = 'Result is ';
+
+add(number1, number2, printResult, resultPhrase);
