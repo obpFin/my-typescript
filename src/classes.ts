73b9e2f1a03d2db9882904646521d31796423710
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-03-30 18:58:35 +0300

    finished classes

--- src/classes.ts
+++ src/classes.ts
@@ -1,14 +1,16 @@
-class Department {
-  // private readonly name: string;
+abstract class Department {
+  static fiscalYear = 2020;
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected id: string, public name: string) {
     // this.name = n;
   }
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`);
+  static createEmployee(name: string) {
+    return { name };
   }
+
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -26,11 +28,41 @@ class ITDepartment extends Department {
     super(id, 'IT');
     this.admins = admins;
   }
+  describe() {
+    console.log(`ITDepartment (${this.id}): ${this.name}`);
+  }
 }
 
 class AccountingDepartment extends Department {
-  constructor(id: string, private reports: string[]) {
+  private lastReport: string;
+  private static instance: AccountingDepartment;
+
+  get mostRecentReport() {
+    if (this.lastReport) return this.lastReport;
+    throw new Error('No new report found');
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) throw new Error('Please specify a value');
+    this.addReport(value);
+  }
+
+  // private constructor can be instanciated inside class, ref. Singleton pattern
+  private constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
+    this.lastReport = reports[0];
+  }
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new AccountingDepartment('d2', []);
+    return this.instance;
+  }
+
+  describe() {
+    console.log(`AccountingDepartment (${this.id}): ${this.name}`);
   }
 
   addEmployee(name: string) {
@@ -42,11 +74,15 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
   getReports() {
     console.log(this.reports);
   }
 }
+
+// call static method
+const employee1 = Department.createEmployee('Anna');
 
 const it = new ITDepartment('d1', ['Max']);
 
@@ -58,9 +94,12 @@ it.printEmployeeInformation();
 
 console.log(it);
 
-const accounting = new AccountingDepartment('d2', []);
+const accounting = AccountingDepartment.getInstance();
+
 accounting.addEmployee('Manu');
 accounting.printEmployeeInformation();
 
 accounting.addReport('Something went wrong...');
 accounting.getReports();
+
+console.log(accounting.mostRecentReport);
