0df0cba0985aea839807b7dfd88f4cb7d4d3b317
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-04-06 21:15:22 +0300

    more generics

--- src/advanced/generics.ts
+++ src/advanced/generics.ts
@@ -15,7 +15,28 @@ function merge<T extends Object, U extends Object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge(
-  { name: 'Max', hobbies: ['sports'] },
-  { age: 23 }
-);
+const mergedObj = merge({ name: 'Max', hobbies: ['sports'] }, { age: 23 });
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descText = 'Got no value. ';
+  if (element.length > 0) {
+    descText = 'Got ' + element.length + ' elements';
+  }
+  return [element, descText];
+}
+
+console.log(countAndDescribe('Hi there'));
+
+// U is key of T
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  console.log(obj[key]);
+}
+
+extractAndConvert({ name: 'Manny' }, 'name');
