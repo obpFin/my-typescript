9c12bae5c8515ec07938f55262832f980b5c011f
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-04-09 21:53:49 +0300

    add logger decorators

--- src/advanced/decorators.ts
+++ src/advanced/decorators.ts
@@ -21,10 +21,59 @@ class Person2 {
   name = 'Max';
 
   constructor() {
-    console.log('creating person object');
+    // console.log('creating person object');
   }
 }
 
-const pers = new Person2();
+function Log(target: any, propertyName: string) {
+  console.log('Property decorator');
+  console.log(target, propertyName);
+}
 
-console.log(pers);
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor decorator');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Method decorator');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string, position: number) {
+  console.log('Parameter decorator');
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  get price() {
+    return this._price;
+  }
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) this._price = val;
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this.price + (1 + tax);
+  }
+}
+
+const product = new Product('title', 224);
