bf111083b725cda0ad86c0bf7551ec99a1d86fee
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-04-20 09:13:17 +0300

    split the app to modules bundled to single file

---
+++ src/drag_n_drop_game/src/components/project-input.ts
@@ -0,0 +1,84 @@
+/// <reference path="base-component.ts" />
+
+namespace App {
+  // Project input class
+  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleinputElement: HTMLInputElement;
+    descriptioninputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+      super('project-input', 'app', true, 'user-input');
+
+      this.titleinputElement = this.element.querySelector(
+        '#title'
+      )! as HTMLInputElement;
+      this.descriptioninputElement = this.element.querySelector(
+        '#description'
+      )! as HTMLInputElement;
+      this.peopleInputElement = this.element.querySelector(
+        '#people'
+      )! as HTMLInputElement;
+
+      this.configure();
+    }
+
+    configure() {
+      this.element.addEventListener('submit', this.submitHandler);
+    }
+
+    renderContent() {}
+
+    private gatherUserInput(): [string, string, number] | void {
+      const enteredTitle = this.titleinputElement.value;
+      const enteredDescription = this.descriptioninputElement.value;
+      const enteredPeople = this.peopleInputElement.value;
+
+      const titleValidatable: Validatable = {
+        value: enteredTitle,
+        required: true,
+      };
+
+      const descriptionValidatable: Validatable = {
+        value: enteredDescription,
+        required: true,
+        minLength: 5,
+      };
+
+      const peopleValidatable: Validatable = {
+        value: +enteredPeople,
+        required: true,
+        min: 1,
+        max: 5,
+      };
+
+      if (
+        !validate(titleValidatable) ||
+        !validate(descriptionValidatable) ||
+        !validate(peopleValidatable)
+      ) {
+        alert('Invalid input!');
+        return;
+      } else {
+        return [enteredTitle, enteredDescription, +enteredPeople];
+      }
+    }
+
+    private clearInput() {
+      this.titleinputElement.value = '';
+      this.descriptioninputElement.value = '';
+      this.peopleInputElement.value = '';
+    }
+
+    @Autobind
+    private submitHandler(e: Event) {
+      e.preventDefault();
+      const userInput = this.gatherUserInput();
+      if (Array.isArray(userInput)) {
+        const [title, desc, people] = userInput;
+        projectState.addProject(title, desc, people);
+        this.clearInput();
+      }
+    }
+  }
+}
---
+++ src/drag_n_drop_game/src/decorators/autobind.ts
@@ -0,0 +1,19 @@
+namespace App {
+  
+  // Decorators
+  export function Autobind(
+    target: any,
+    methodName: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjustedDescriptor;
+  }
+}
