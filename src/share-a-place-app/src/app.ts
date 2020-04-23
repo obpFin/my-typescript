ae1115c09dbf2656e68c390ae3493bbf92e2f14d
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-04-23 08:40:19 +0300

    app frame

--- src/share-a-place-app/src/app.ts
+++ src/share-a-place-app/src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+import { GOOGLE_KEY } from './keys';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(e: Event) {
+  e.preventDefault();
+  const enteredAddress = addressInput.value;
+}
+
+form?.addEventListener('submit', searchAddressHandler);
