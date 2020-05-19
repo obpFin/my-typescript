c1e164621c91292ec198f57ec7ab1f75f3b4b3fc
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-05-19 19:48:32 +0300

    show todos in the app

--- src/my-ts-react-app/src/App.tsx
+++ src/my-ts-react-app/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+import TodoList from './components/TodoList';
 
+function App() {
+  const todos = [{ id: 't1', text: 'Finish the course' }];
+
+  return (
+    <div className='App'>
+      <TodoList items={todos} />
     </div>
   );
 }
