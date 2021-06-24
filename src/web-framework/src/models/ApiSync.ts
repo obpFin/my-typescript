febbfaab2dfcc1aab28e0fab7e4c520d98811770
Oskari Peltonen <oskari.peltonen@outlook.com>
2021-06-24 20:56:33 +0300

    added Apisync class

---
+++ src/web-framework/src/models/ApiSync.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosPromise } from 'axios';
+
+interface hasId {
+  id?: number;
+}
+
+export class ApiSync<T extends hasId> {
+  constructor(public rooturl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rooturl}/${id}`);
+  }
+
+  save(data: T): AxiosPromise {
+    const { id } = data;
+    if (id) {
+      return axios.put(`${this.rooturl}/${id}`, data);
+    } else {
+      return axios.post(this.rooturl, data);
+    }
+  }
+}
