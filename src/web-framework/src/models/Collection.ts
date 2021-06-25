f1372715ab39518bdc6353691955a6fe57f09c93
Oskari Peltonen <oskari.peltonen@outlook.com>
2021-06-25 10:53:10 +0300

    added Collection class

---
+++ src/web-framework/src/models/Collection.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
+
+export class Collection<T, K> {
+  models: T[] = [];
+  events: Eventing = new Eventing();
+
+  constructor(public rootUrl: string, public deSerialize: (json: K) => T) {}
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  fetch() {
+    axios.get(this.rootUrl).then((res: AxiosResponse) => {
+      res.data.forEach((value: K) => {
+        this.models.push(this.deSerialize(value));
+      });
+    });
+    this.trigger('change');
+  }
+}
