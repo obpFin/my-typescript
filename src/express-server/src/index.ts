2e39f60d37543d8453a81dd92fd5bc8b17a2b9b7
Oskari Peltonen <oskari.peltonen@outlook.com>
2021-06-29 21:12:48 +0300

    add routes

--- src/express-server/src/index.ts
+++ src/express-server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('init');
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['jlhasf'] }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
