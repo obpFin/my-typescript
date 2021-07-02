cef01be06599fca6fed2567a686dda631d5ac7a9
Oskari Peltonen <oskari.peltonen@outlook.com>
2021-07-02 21:51:50 +0300

    added login controller with decorators

--- src/express-server/src/routes/loginRoutes.ts
+++ src/express-server/src/routes/loginRoutes.ts
@@ -16,22 +16,6 @@ function requireAuth(req: Request, res: Response, next: NextFunction) {
 
 const router = Router();
 
-router.get('/login', (req: Request, res: Response) => {
-  res.send(`
-    <form method="POST">
-      <div>
-        <label>Email</label>
-        <input name="email" />
-      </div>
-      <div>
-      <label>Password</label>
-      <input type="password" name="password" />
-    </div>
-    <button>Submit</button>
-    </form>
-  `);
-});
-
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
@@ -44,7 +28,6 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
 });
 
 router.get('/', (req: Request, res: Response) => {
-  console.log(req.session);
   if (req.session && req.session.loggedIn) {
     res.send(`
       <div>
