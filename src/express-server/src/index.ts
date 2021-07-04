1767b00dba492d814ad2b9f0949ee492a08213b0
Oskari Peltonen <oskari.peltonen@outlook.com>
2021-07-04 16:16:08 +0300

    add decorators

--- src/express-server/src/index.ts
+++ src/express-server/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
 import cookieSession from 'cookie-session';
-import { router as controllerRouter } from './controllers/decorators/controller';
+import { AppRouter } from './appRouter';
 import './controllers/LoginController';
 
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['jlhasf'] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
