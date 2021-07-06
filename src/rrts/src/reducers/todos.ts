6cbab5679cb8af1354e6d0500a0ef0f6c127b8a4
Oskari Peltonen <oskari.peltonen@outlook.com>
2021-07-06 16:37:21 +0300

    todo actions

--- src/rrts/src/reducers/todos.ts
+++ src/rrts/src/reducers/todos.ts
@@ -1,9 +1,10 @@
-import { Todo, FetchTodosAction } from '../actions';
-import { ActionTypes } from '../actions/types';
+import { Todo, Action, ActionTypes } from '../actions';
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
+      return action.payload;
+    case ActionTypes.deleteTodo:
       return action.payload;
     default:
       return state;
